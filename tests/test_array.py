d316023ccdf4412d22f1e74f93cdad3a9715ed79
Daw <da.daw648@gmail.com>
2025-11-16 16:42:14 -0500

    tests

--- tests/test_array.py
+++ tests/test_array.py
@@ -1,6 +1,23 @@
 from Forge import Array
+import pytest
 
 def test():
     """name"""
     a = Array([[1, 2, 3], [4,5,6]])
     assert a.shape == (2,3)
+
+def test_addition():
+    a1 = Array([[1.0, 2.0], [3.0, 4.0]])
+    a2 = Array([[4.0, 5.0], [6.0, 7.0]])
+    result = a1 + a2
+
+    assert result.list() == [[5.0, 7.0], [9.0, 11.0]]
+
+def test_wrong_size():
+    with pytest.raises(ValueError):
+        Array([[1, 2, 3], []])
+
+def test_empty_array():
+    arr = Array([])
+    assert arr.shape == (0,)
+    assert arr.list() == []
