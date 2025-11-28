93c84c3450b279067115ccb6cc8ab7af9fca24ea
Daw <da.daw648@gmail.com>
2025-11-27 21:04:30 -0500

    test

--- cpp/include/array_add.h
+++ cpp/include/array_add.h
@@ -4,3 +4,10 @@
 
 std::shared_ptr<ArrayHandle> add_arrays_cpp(const std::shared_ptr<ArrayHandle>& A, 
     const std::shared_ptr<ArrayHandle>& B);
+
+enum class ArrayOperationType : int {
+    ADD = 0,
+    SUB = 1,
+    MULT = 2,
+    DIV = 3
+}
--- cpp/src/array_add.mm
+++ cpp/src/array_add.mm
@@ -2,13 +2,6 @@
 
 #include "../include/array_add.h"
 
-enum class ArrayOperationType : int {
-    ADD = 0,
-    SUB = 1,
-    MULT = 2,
-    DIV = 3
-}
-
 std::shared_ptr<ArrayHandle> operations_arrays_cpp(
     const std::shared_ptr<ArrayHandle>& A, 
     const std::shared_ptr<ArrayHandle>& B,
