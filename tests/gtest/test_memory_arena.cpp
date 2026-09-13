da7c6ff6a8fa226ab0f4ff709913511e16c7be50
Kellen Sun <kellensun2020@gmail.com>
2026-09-13 12:37:57 -0400

    codegen tests

--- tests/gtest/test_memory_arena.cpp
+++ tests/gtest/test_memory_arena.cpp
@@ -1,61 +1,19 @@
 #include <gtest/gtest.h>
 
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <vector>
-
+#include "graph_io.h"
 #include "../../cpp/include/memory_arena.h"
 
-template <typename T>
-std::vector<T> parse_line_to_vector(const std::string& line) {
-    std::istringstream iss(line);
-    std::vector<T> vec;
-    T val;
-    while (iss >> val) vec.push_back(val);
-    return vec;
-}
+#ifndef FORGE_GTEST_DIR
+#define FORGE_GTEST_DIR "../tests/gtest"
+#endif
 
-std::vector<std::string> read_file_lines(const std::string& filepath) {
-    std::ifstream file(filepath);
-    if (!file.is_open()) {
-        throw std::runtime_error("Failed to open test file: " + filepath);
-    }
-    std::vector<std::string> lines;
-    std::string line;
-    while (std::getline(file, line)) {
-        lines.push_back(line);
-    }
-    return lines;
-}
-
-class MemoryArenaTest : public ::testing::TestWithParam<std::string> {
-   protected:
-    Graph parse_graph(const std::string& filepath) {
-        auto lines = read_file_lines(filepath);
-        int output_index = std::stoi(lines.back());
-        lines.pop_back();
-
-        std::vector<Node> nodes;
-        for (size_t i = 0; i < lines.size(); i += 6) {
-            Node n;
-            n.op = static_cast<OpCode>(std::stoi(lines[i]));
-            n.inputs = parse_line_to_vector<int>(lines[i + 1]);
-            n.shape = parse_line_to_vector<int64_t>(lines[i + 2]);
-            n.strides = parse_line_to_vector<int64_t>(lines[i + 3]);
-            n.offset = std::stoll(lines[i + 4]);
-            n.args = parse_line_to_vector<int64_t>(lines[i + 5]);
-            nodes.push_back(n);
-        }
-        return Graph(nodes, output_index);
-    }
-};
+class MemoryArenaTest : public ::testing::TestWithParam<std::string> {};
 
 TEST_P(MemoryArenaTest, TestAllocation) {
     std::string test_name = GetParam();
-
-    std::string in_path = "../tests/gtest/memory_arena_tests/" + test_name + ".in";
-    std::string out_path = "../tests/gtest/memory_arena_tests/" + test_name + ".out";
+    std::string dir = std::string(FORGE_GTEST_DIR) + "/memory_arena_tests/";
+    std::string in_path = dir + test_name + ".in";
+    std::string out_path = dir + test_name + ".out";
 
     Graph g = parse_graph(in_path);
     auto lines = read_file_lines(out_path);
