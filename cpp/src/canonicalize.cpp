03347e462784acdde35c62bc523891028a2494e6
Kellen Sun <kellensun2020@gmail.com>
2026-09-13 19:51:06 -0400

    the pass itself

---
+++ cpp/src/canonicalize.cpp
@@ -0,0 +1,28 @@
+#include "../include/pass.h"
+
+static bool is_layout_op(OpCode op) {
+    return op == OpCode::VIEW || op == OpCode::RESHAPE || op == OpCode::TRANSPOSE;
+}
+
+static bool is_identity_layout(const Node& node, const Node& src) {
+    return is_layout_op(node.op) && node.inputs.size() == 1 && node.shape == src.shape &&
+           node.strides == src.strides && node.offset == src.offset;
+}
+
+static int skip_identity(const IR& ir, int i) {
+    while (i >= 0 && i < static_cast<int>(ir.nodes.size())) {
+        const Node& node = ir.nodes[i];
+        if (node.inputs.size() != 1) break;
+        const Node& src = ir.nodes[node.inputs[0]];
+        if (!is_identity_layout(node, src)) break;
+        i = node.inputs[0];
+    }
+    return i;
+}
+
+void CanonicalizePass::run(IR& ir) {
+    for (auto& node : ir.nodes) {
+        for (int& inp : node.inputs) inp = skip_identity(ir, inp);
+    }
+    ir.output_index = skip_identity(ir, ir.output_index);
+}
