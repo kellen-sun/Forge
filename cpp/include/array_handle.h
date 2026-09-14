cbf0244b3580e6014789c81e65181e50dc1562a3
Kellen Sun <kellensun2020@gmail.com>
2026-09-13 20:09:41 -0400

    move matmul broadcast to helper

--- cpp/include/array_handle.h
+++ cpp/include/array_handle.h
@@ -77,6 +77,9 @@ std::shared_ptr<ArrayHandle> array_reshape(const std::shared_ptr<ArrayHandle>& h
 std::vector<int64_t> broadcast_shapes(std::span<const int64_t>&& a_shape,
                                       std::span<const int64_t>&& b_shape);
 
+std::vector<int64_t> matmul_output_shape(const std::vector<int64_t>& a_shape,
+                                         const std::vector<int64_t>& b_shape);
+
 std::vector<int64_t> get_bcast_strides(const std::vector<int64_t>& shape,
                                        const std::vector<int64_t>& strides,
                                        const std::vector<int64_t>& final_shape);
